c7cbb5cca00cea882a8ea9d953be20bb90d16f59
mrcdr <mrcdr@users.noreply.github.com>
2020-10-25 02:21:39 +0900

    lambda-lanczos version changed

--- docs/lambda-lanczos/search/all_6.js
+++ docs/lambda-lanczos/search/all_6.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['norm_21',['norm',['../namespacelambda__lanczos_1_1util.html#adb626f86965a358c0966af537e4f8fcb',1,'lambda_lanczos::util']]],
   ['normalize_22',['normalize',['../namespacelambda__lanczos_1_1util.html#a0e17f6a13736c7950e32b564492dce3b',1,'lambda_lanczos::util']]],
-  ['num_5fof_5feigs_5fsmaller_5fthan_23',['num_of_eigs_smaller_than',['../classlambda__lanczos_1_1LambdaLanczos.html#a466ee6c25182b47c9f52b28249d451ef',1,'lambda_lanczos::LambdaLanczos']]]
+  ['num_5fof_5feigs_5fsmaller_5fthan_23',['num_of_eigs_smaller_than',['../classlambda__lanczos_1_1LambdaLanczos.html#a11c5ad21516bce860357f19b95a823d1',1,'lambda_lanczos::LambdaLanczos']]]
 ];
--- docs/lambda-lanczos/search/functions_6.js
+++ docs/lambda-lanczos/search/functions_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['run_59',['run',['../classlambda__lanczos_1_1LambdaLanczos.html#aae5f01a9613aa4660ef8e9fbb2fe3444',1,'lambda_lanczos::LambdaLanczos']]]
+  ['run_59',['run',['../classlambda__lanczos_1_1LambdaLanczos.html#aae5f01a9613aa4660ef8e9fbb2fe3444',1,'lambda_lanczos::LambdaLanczos::run(real_t&lt; T &gt; &amp;eigvalue, std::vector&lt; T &gt; &amp;eigvec) const'],['../classlambda__lanczos_1_1LambdaLanczos.html#a1ed2c486ffbf0be41e069fdbe5f37a0b',1,'lambda_lanczos::LambdaLanczos::run() const']]]
 ];
